3c4a0a396678a257043942db429a105662fb826a
Mihajlo <mihajlovicent@gmail.com>
2019-12-23 09:25:03 +0100

    [NF] Initial GUI

--- haxball-client/main.cpp
+++ haxball-client/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   MainWindow w;
+  w.setWindowFlag(Qt::FramelessWindowHint);
+  w.setWindowState(Qt::WindowFullScreen);
   w.show();
   return a.exec();
 }
--- haxball-client/mainwindow.cpp
+++ haxball-client/mainwindow.cpp
@@ -13,3 +13,8 @@ MainWindow::~MainWindow()
   delete ui;
 }
 
+
+void MainWindow::on_exitButton_clicked()
+{
+    QApplication::exit();
+}
